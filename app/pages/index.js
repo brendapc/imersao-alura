316eea17eeb378b2002e7c61d6802ab2d0c07dbf
Brenda Camara <brendacamara@outlook.com>
2021-08-08 20:38:47 -0300

    feat: create-next-app

---
+++ app/pages/index.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Box = styled.div`
+  background: #fff;
+  border-radius: 8px;
+`
+
+export default function Home() {
+  return (
+    <main>
+      <Box />
+
+    </main>
+  )
+}
